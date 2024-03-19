7227e8ad4c1b7222b9986a97470b9d9e5153eba2
Robert Wagner <robert.wagner@octopus.com>
2024-03-19 13:18:41 +1000

    Referenced MySqlConnector

--- src/dbup-mysql/MySqlScriptExecutor.cs
+++ src/dbup-mysql/MySqlScriptExecutor.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using DbUp.Engine;
 using DbUp.Engine.Output;
@@ -41,7 +41,7 @@ namespace DbUp.MySql
             }
             catch (MySqlException exception)
             {
-                var code = exception.Code;
+                var code = exception.ErrorCode;
                 Log().LogInformation("MySql exception has occurred in script: '{0}'", script.Name);
                 Log().LogError("Script block number: {0}; MySql error code: {1}; Number {2}; Message: {3}", index, code, exception.Number, exception.Message);
                 Log().LogError(exception.ToString());
